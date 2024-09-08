df32105b14c8aa7c8fba41a392a3b2832388b656
Sarath S <sarathsuryasss48@gmail.com>
2024-09-08 12:11:15 +0530

    third commit

--- src/app/features/user/components/upload-video/upload-video.component.ts
+++ src/app/features/user/components/upload-video/upload-video.component.ts
@@ -8,18 +8,42 @@ import { MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./upload-video.component.css']
 })
 export class UploadVideoComponent {
-name: any;
-animal: any;
+  video: File | null = null; // Variable to store file
+  url: string | ArrayBuffer | null | undefined
+  videoData!: File;
+  videoName!:string;
+  thumbNailData!:File
+
 constructor(public dialogRef: MatDialogRef<UploadVideoComponent>,private _formBuilder: FormBuilder) {}
+videoFile(event:Event) {
+  const FILE = event.target as HTMLInputElement
+  if (FILE.files && FILE.files[0]) {
+    this.videoData = FILE.files[0];
+    this.videoName = this.videoData.name
+  }
+}
+
+thumbNail(event:Event) {
+  const FILE = event.target as HTMLInputElement
+  if (FILE.files && FILE.files[0]) {
+    this.thumbNailData = FILE.files[0];
+  }
+}
+
 onNoClick(): void {
     this.dialogRef.close();
   }
   firstFormGroup = this._formBuilder.group({
-    firstCtrl: ['', Validators.required],
+    firstCtrl: ['',],
   });
   secondFormGroup = this._formBuilder.group({
-    secondCtrl: ['', Validators.required],
+    secondCtrl: ['',],
   });
-  isLinear = false;
+  thirdFormGroup = this._formBuilder.group({
+    title:['',Validators.required],
+    description:['',Validators.required]
+  })
+
+  isLinear = true;
 
 }
