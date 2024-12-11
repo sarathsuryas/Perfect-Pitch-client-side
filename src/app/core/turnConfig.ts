9b14f1fc69381e30e00275624f381d4476c20e56
Sarath S <sarathsuryasss48@gmail.com>
2024-12-11 13:26:28 +0530

    added twilio service

--- src/app/core/turnConfig.ts
+++ src/app/core/turnConfig.ts
@@ -1,8 +1,7 @@
 export const  iceConfiguration = {
-
-  iceServers: [{   urls: [ "stun:stun:global.stun.twilio.com:3478?transport=udp" ]}]
-
+  ice_servers: [{url: "stun:global.stun.twilio.com:3478", urls: "stun:global.stun.twilio.com:3478"}, {url: "turn:global.turn.twilio.com:3478?transport=udp", username: "978b4e0aa4bd05e0e6f47f1f68a7b5edf9fe1e6bbdc777a1042b8924bf3c5a2e", urls: "turn:global.turn.twilio.com:3478?transport=udp", credential: "cYV04JfTfKFBDwAwRMIoYYRCNQWsccvXd6V/7vJ2zLI="}, {url: "turn:global.turn.twilio.com:3478?transport=tcp", username: "978b4e0aa4bd05e0e6f47f1f68a7b5edf9fe1e6bbdc777a1042b8924bf3c5a2e", urls: "turn:global.turn.twilio.com:3478?transport=tcp", credential: "cYV04JfTfKFBDwAwRMIoYYRCNQWsccvXd6V/7vJ2zLI="}, {url: "turn:global.turn.twilio.com:443?transport=tcp", username: "978b4e0aa4bd05e0e6f47f1f68a7b5edf9fe1e6bbdc777a1042b8924bf3c5a2e", urls: "turn:global.turn.twilio.com:443?transport=tcp", credential: "cYV04JfTfKFBDwAwRMIoYYRCNQWsccvXd6V/7vJ2zLI="}]
 };
+
 // export const  iceConfiguration = {
 
 //   iceServers: [{   urls: [ "stun:bn-turn2.xirsys.com" ]}, {   username: "o8_s2lbVKiqxpNa5Ntw5kG_h7g9zYj-AbK49RHWtnH26b_exoUgSkD5MrvzAQkpMAAAAAGcrwiBzYXJhdGhz",   credential: "90886c3c-9c74-11ef-8e6e-0242ac140004",   urls: [       "turn:bn-turn2.xirsys.com:80?transport=udp",       "turn:bn-turn2.xirsys.com:3478?transport=udp",       "turn:bn-turn2.xirsys.com:80?transport=tcp",       "turn:bn-turn2.xirsys.com:3478?transport=tcp",       "turns:bn-turn2.xirsys.com:443?transport=tcp",       "turns:bn-turn2.xirsys.com:5349?transport=tcp"   ]}]
