279e6b3254face0cac9cb33091d6d1c354531144
sarathsuryas <sarathsuryasss48@gmail.com>
2025-03-17 08:54:45 +0530

    turn server changed

--- src/app/test/viewer/viewer.component.ts
+++ src/app/test/viewer/viewer.component.ts
@@ -106,7 +106,31 @@ export class ViewerComponent {
 
   setupPeerConnection(offer: RTCSessionDescriptionInit) {
     const configuration = {
-      iceServers: [{   urls: [ "stun:bn-turn2.xirsys.com" ]}, {   username: "o8_s2lbVKiqxpNa5Ntw5kG_h7g9zYj-AbK49RHWtnH26b_exoUgSkD5MrvzAQkpMAAAAAGcrwiBzYXJhdGhz",   credential: "90886c3c-9c74-11ef-8e6e-0242ac140004",   urls: [       "turn:bn-turn2.xirsys.com:80?transport=udp",       "turn:bn-turn2.xirsys.com:3478?transport=udp",       "turn:bn-turn2.xirsys.com:80?transport=tcp",       "turn:bn-turn2.xirsys.com:3478?transport=tcp",       "turns:bn-turn2.xirsys.com:443?transport=tcp",       "turns:bn-turn2.xirsys.com:5349?transport=tcp"   ]}]
+      iceServers: [
+        {
+          urls: "stun:stun.relay.metered.ca:80",
+        },
+        {
+          urls: "turn:global.relay.metered.ca:80",
+          username: "836c17083ecba16b626af6f7",
+          credential: "j/Du96pT1PjJXgP/",
+        },
+        {
+          urls: "turn:global.relay.metered.ca:80?transport=tcp",
+          username: "836c17083ecba16b626af6f7",
+          credential: "j/Du96pT1PjJXgP/",
+        },
+        {
+          urls: "turn:global.relay.metered.ca:443",
+          username: "836c17083ecba16b626af6f7",
+          credential: "j/Du96pT1PjJXgP/",
+        },
+        {
+          urls: "turns:global.relay.metered.ca:443?transport=tcp",
+          username: "836c17083ecba16b626af6f7",
+          credential: "j/Du96pT1PjJXgP/",
+        },
+    ]
     };
 
     // Close any existing connection
