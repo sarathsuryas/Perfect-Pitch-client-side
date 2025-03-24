b8e38c3a35dfdbd9277e0a2d1bef5b1565d2db49
sarathsuryas <sarathsuryasss48@gmail.com>
2025-03-24 14:13:06 +0530

    updated twilio turn server

--- src/app/features/user/components/live-streaming/live-streaming.component.ts
+++ src/app/features/user/components/live-streaming/live-streaming.component.ts
@@ -133,31 +133,26 @@ export class LiveStreamingComponent implements OnDestroy {
 
   setupPeerConnection(offer:RTCSessionDescriptionInit) {
     const configuration = {
-        iceServers: [
-            {
-                urls: "stun:stun.relay.metered.ca:80",
-            },
-            {
-                urls: "turn:global.relay.metered.ca:80",
-                username: "836c17083ecba16b626af6f7",
-                credential: "j/Du96pT1PjJXgP/",
-            },
-            {
-                urls: "turn:global.relay.metered.ca:80?transport=tcp",
-                username: "836c17083ecba16b626af6f7",
-                credential: "j/Du96pT1PjJXgP/",
-            },
-            {
-                urls: "turn:global.relay.metered.ca:443",
-                username: "836c17083ecba16b626af6f7",
-                credential: "j/Du96pT1PjJXgP/",
-            },
-            {
-                urls: "turns:global.relay.metered.ca:443?transport=tcp",
-                username: "836c17083ecba16b626af6f7",
-                credential: "j/Du96pT1PjJXgP/",
-            },
-        ],
+      iceServers: [
+        {
+          urls: "stun:global.stun.twilio.com:3478"
+        },
+        {
+          username: "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
+          credential: "tE2DajzSJwnsSbc123",
+          urls: "turn:global.turn.twilio.com:3478?transport=udp"
+        },
+        {
+          username: "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
+          credential: "tE2DajzSJwnsSbc123",
+          urls: "turn:global.turn.twilio.com:3478?transport=tcp"
+        },
+        {
+          username: "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
+          credential: "tE2DajzSJwnsSbc123",
+          urls: "turn:global.turn.twilio.com:443?transport=tcp"
+        }
+      ]
     };
   
  const remoteVideo = document.getElementById('video') as HTMLVideoElement
