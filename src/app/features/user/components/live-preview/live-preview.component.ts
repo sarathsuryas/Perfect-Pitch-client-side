bd39c5708d0aae30abfc77ca5b71c477e82fc3a0
sarathsuryas <sarathsuryasss48@gmail.com>
2025-03-24 14:22:51 +0530

    updated metered relay

--- src/app/features/user/components/live-preview/live-preview.component.ts
+++ src/app/features/user/components/live-preview/live-preview.component.ts
@@ -138,26 +138,31 @@ export class LivePreviewComponent {
 
   setupPeerConnection() {
     const configuration = {
-      iceServers: [
-        {
-          urls: "stun:global.stun.twilio.com:3478"
-        },
-        {
-          username: "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
-          credential: "tE2DajzSJwnsSbc123",
-          urls: "turn:global.turn.twilio.com:3478?transport=udp"
-        },
-        {
-          username: "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
-          credential: "tE2DajzSJwnsSbc123",
-          urls: "turn:global.turn.twilio.com:3478?transport=tcp"
-        },
-        {
-          username: "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
-          credential: "tE2DajzSJwnsSbc123",
-          urls: "turn:global.turn.twilio.com:443?transport=tcp"
-        }
-      ]
+        iceServers: [
+            {
+                urls: "stun:stun.relay.metered.ca:80",
+            },
+            {
+                urls: "turn:global.relay.metered.ca:80",
+                username: "836c17083ecba16b626af6f7",
+                credential: "j/Du96pT1PjJXgP/",
+            },
+            {
+                urls: "turn:global.relay.metered.ca:80?transport=tcp",
+                username: "836c17083ecba16b626af6f7",
+                credential: "j/Du96pT1PjJXgP/",
+            },
+            {
+                urls: "turn:global.relay.metered.ca:443",
+                username: "836c17083ecba16b626af6f7",
+                credential: "j/Du96pT1PjJXgP/",
+            },
+            {
+                urls: "turns:global.relay.metered.ca:443?transport=tcp",
+                username: "836c17083ecba16b626af6f7",
+                credential: "j/Du96pT1PjJXgP/",
+            },
+        ],
     };
 
     this.peerConnection = new RTCPeerConnection(configuration);
